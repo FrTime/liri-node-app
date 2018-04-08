588066ebc20a64133c9399a5aab9678ec290bb92
Tim <timothy.mace@outlook.com>
2018-04-08 13:03:13 -0400

    spotify command working

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -23,7 +23,7 @@ for (i = 3; i < pArgv.length; i++) {
   } else {
     searchParam = searchParam + pArgv[i];
   }
-}
+};
 
 // Defining functions for entered commands
 
@@ -34,7 +34,7 @@ function doWhatItSays() {
     var randomArr = data.split(",");
     spotifyThisSong(randomArr[1]);
   });
-}
+};
 
 function myTweets() {
   // Displays the 20 most recent tweets of the user
@@ -53,7 +53,28 @@ function myTweets() {
       console.log("\n-------------------- \n");
     }
   });
-}
+};
+
+function spotifyThisSong(title) {
+  // Displays the closest result to the entered search terms
+  // If nothing is entered, "The Sign" by Ace of Bass is searched for instead
+  spotify.search({ type: "track", query: title, limit: 1 }, function(
+    error,
+    data
+  ) {
+    if (error) throw error;
+    for (var i = 0; i < data.tracks.items.length; i++) {
+      var song = data.tracks.items[i];
+      console.log(
+        `Artist: ${song.artists[0].name} \nTrack Title: ${
+          song.name
+        } \nSpotify Preview Link: ${song.preview_url} \nAlbum: ${
+          song.album.name
+        } (Released ${song.album.release_date})`
+      );
+    }
+  });
+};
 
 // Setting actions depending on command entered
 
@@ -72,7 +93,7 @@ switch (command) {
       console.log(
         "No search term(s) entered. We hope you enjoy our selection:\n"
       );
-      spotifyThisSong("The Sign");
+      spotifyThisSong("The Sign Ace of Base");
     }
     break;
 
